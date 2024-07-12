aed207b8345f63147b4ff6a890f50ed3be484db2
Eduardo Santos <eduardo.experimental@gmail.com>
2024-07-11 23:41:43 -0300

    1[Python]: Improve two-pointers solution

--- python3/1/b.py
+++ python3/1/b.py
@@ -1,23 +1,20 @@
-# Algorithm/technique: sorting
+# Algorithm/technique: sorting, two-pointers
 # Time complexity:     O(n lb n)
 # Auxiliary space:     O(n)
 # Optimal:             no
 
 class Solution:
     def twoSum(self, nums: list[int], target: int) -> list[int]:
-        snums: list[int] = sorted(nums)
+        snums: list[enumerate] = sorted(enumerate(nums), key=lambda n: n[1])
 
         i: int = 0
         j: int = len(nums) - 1
         total: int
         res: list[int]
         while i < j:
-            total = snums[i] + snums[j]
+            total = snums[i][1] + snums[j][1]
             if total == target:
-                res = [nums.index(snums[i]), nums.index(snums[j])]
-                if res[0] == res[1]:
-                    res[1] = nums.index(snums[j], res[0] + 1)
-                return res
+                return [snums[i][0], snums[j][0]]
             elif total < target:
                 i += 1
             else:
