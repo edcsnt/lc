6f564cedc41b5cee263695165f5a1a7ef50906b4
Eduardo Santos <33993373+edcsnt@users.noreply.github.com>
2025-09-14 20:27:40 -0300

    remove license, add copyright notice and remove CI

--- 0242.py
+++ 0242.py
@@ -1,20 +1,4 @@
-# Copyright (C) 2025 Eduardo Santos <eduardo.experimental@gmail.com>
-#
-# This file is part of edcsnt/lc.
-#
-# edcsnt/lc is free software: you can redistribute it and/or modify it under
-# the terms of the GNU General Public License as published by the Free Software
-# Foundation, either version 3 of the License, or (at your option) any later
-# version.
-#
-# edcsnt/lc is distributed in the hope that it will be useful, but WITHOUT ANY
-# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
-# A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License along with
-# edcsnt/lc. If not, see <https://www.gnu.org/licenses/>.
-#
-
+# Copyright 2025 Eduardo Santos. All Rights Reserved.
 class Solution:
     def all_zeros(self, ls: list[int]) -> bool:
         for i in ls:
